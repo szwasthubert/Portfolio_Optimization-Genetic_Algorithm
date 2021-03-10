f5870d45d8b2ba1fbfb11a3fd3896db3460c69b8
Bartłomiej Klocek <barthap10@gmail.com>
2021-03-10 15:00:29 +0100

    App template

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Minimalizuje funkcje: f(x1,x2,x3) = (x1-0.2)^2 + (x2-0.8)^2 + (x3-0.5)^2")
+
+import numpy as np
+from datetime import datetime
+
+import listtools
+from config import config as cfg
+from genetic.operators import OperatorsImpl
+from genetic.algorithm import GeneticAlgorithm
+
+
+simulation = GeneticAlgorithm(cfg, OperatorsImpl(cfg))
+results = simulation.run()
+
+best_index = listtools.max_index_in_list(results.max_values)
+best_chromosome = results.champion_chromosomes[best_index]
+
+print('Best iteration:', best_index)
+print(best_chromosome)
+print("Algorithm total time: {:.2f}s".format(results.totalTime))
+
