a4c10ca9c872a76dd7fcc70b1292655007499be6
freestylofil <freyfilip3@gmail.com>
2021-03-10 15:03:13 +0100

    crypro parsing pathfix

--- helpers/prepareData.py
+++ helpers/prepareData.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 
 def readCrypto():
-    dfBitcoin = pd.read_csv("../datasets/crypto/bitcoin.csv", parse_dates=True, index_col=0)
-    dfEtherum = pd.read_csv("../datasets/crypto/etherum.csv", parse_dates=True, index_col=0)
-    dfLitecoin = pd.read_csv("../datasets/crypto/litecoin.csv", parse_dates=True, index_col=0)
-    dfXRP = pd.read_csv("../datasets/crypto/xrp.csv", parse_dates=True, index_col=0)
+    dfBitcoin = pd.read_csv("data/crypto/bitcoin.csv", parse_dates=True, index_col=0)
+    dfEtherum = pd.read_csv("data/crypto/etherum.csv", parse_dates=True, index_col=0)
+    dfLitecoin = pd.read_csv("data/crypto/litecoin.csv", parse_dates=True, index_col=0)
+    dfXRP = pd.read_csv("data/crypto/xrp.csv", parse_dates=True, index_col=0)
 
-    df = pd.DataFrame({'bitcoin': dfBitcoin['open'], 'ethereum': dfEtherum['open'], 'litecoin':dfLitecoin['open'], 'xrp':dfXRP['open']}, index=dfBitcoin.index)
-    return df
+    return pd.DataFrame({'bitcoin': dfBitcoin['open'], 'ethereum': dfEtherum['open'], 'litecoin':dfLitecoin['open'], 'xrp':dfXRP['open']}, index=dfBitcoin.index)
