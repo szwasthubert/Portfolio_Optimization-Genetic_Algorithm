7b1ca5ba47993506e92bd3dc169f620f0f058797
freestylofil <freyfilip3@gmail.com>
2021-03-10 22:49:06 +0100

    portfolio std and expected return

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,8 +1,35 @@
+import math
+
+
 def calculateMean(dataframe):
     return dataframe.mean(axis=0, skipna=True)
 
-def calculateSTD(dataframe):
+
+def calculateStd(dataframe):
     return dataframe.std(axis=0, skipna=True)
+
 
 def calculateCovarianceMatrix(dataframe):
     return dataframe.cov()
+
+
+def expectedPortfolioReturn(portfolioWeightsDict, meanReturnsDataset):
+    expectedReturn = 0
+    for key, value in portfolioWeightsDict.items():
+        expectedReturn += value * meanReturnsDataset[key]
+    return expectedReturn
+
+
+def portfolioStd(portfolioWeightsDict, covarianceDataset):
+    n = len(portfolioWeightsDict)
+    portfolioKeys = portfolioWeightsDict.keys()
+    variance = 0
+    for key, value in portfolioWeightsDict.items():
+        variance += covarianceDataset.loc[key][key] * value ** 2
+
+    for i in range(n):
+        for j in range(i + 1, n + 1):
+            variance += 2 * portfolioWeightsDict[portfolioKeys[i]] * portfolioWeightsDict[portfolioKeys[j]] * \
+                        covarianceDataset.iloc[portfolioKeys[i]][portfolioKeys[j]]
+
+    return math.sqrt(variance)
--- helpers/prepareData.py
+++ helpers/prepareData.py
@@ -1,10 +1,24 @@
 import pandas as pd
-import numpy as np
 
 def readCrypto():
-    dfBitcoin = pd.read_csv("data/crypto/bitcoin.csv", parse_dates=True, index_col=0)
-    dfEtherum = pd.read_csv("data/crypto/etherum.csv", parse_dates=True, index_col=0)
-    dfLitecoin = pd.read_csv("data/crypto/litecoin.csv", parse_dates=True, index_col=0)
-    dfXRP = pd.read_csv("data/crypto/xrp.csv", parse_dates=True, index_col=0)
+    dfBitcoin = pd.read_csv("data/crypto/bitcoin.csv", parse_dates=True)
+    dfEtherum = pd.read_csv("data/crypto/etherum.csv", parse_dates=True)
+    dfLitecoin = pd.read_csv("data/crypto/litecoin.csv", parse_dates=True)
+    dfXRP = pd.read_csv("data/crypto/xrp.csv", parse_dates=True)
 
-    return pd.DataFrame({'bitcoin': dfBitcoin['open'], 'ethereum': dfEtherum['open'], 'litecoin':dfLitecoin['open'], 'xrp':dfXRP['open']}, index=dfBitcoin.index)
+    return pd.DataFrame({'bitcoin': dfBitcoin['open'], 'ethereum': dfEtherum['open'], 'litecoin':dfLitecoin['open'], 'xrp':dfXRP['open']})
+
+def convertClosesToReturns(dataframe):
+    return dataframe.diff().drop([0], axis='rows')
+
+def returnCorrelationMatrix(covDataframe, stdDataframe):
+
+    corrMatrix = covDataframe.copy()
+
+    for colname in corrMatrix.columns:
+        corrMatrix[colname] /= stdDataframe[colname]
+
+    for rowname in corrMatrix.index.values:
+        corrMatrix.loc[rowname] /= stdDataframe[rowname]
+
+    return corrMatrix
