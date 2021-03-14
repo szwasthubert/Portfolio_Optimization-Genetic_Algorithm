a39236ac65d22f210210ebc0f5ea6383446a82c2
Bartłomiej Klocek <barthap10@gmail.com>
2021-03-14 12:38:30 +0100

    Plots for algorithm results

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import listtools
 import numpy as np
-from config import config as cfg
+from config import config as cfg, Config
 from genetic.operators import OperatorsImpl
-from genetic.algorithm import GeneticAlgorithm, AlgorithmParams
+from genetic.algorithm import GeneticAlgorithm, AlgorithmParams, AlgorithmResult
+
+import matplotlib.pyplot as plt
 
 # przykladowe liczby z dupy wziete
 params_crypto = AlgorithmParams(lam=0.5,
@@ -15,11 +17,53 @@ simulation = GeneticAlgorithm(cfg, OperatorsImpl(cfg), params_crypto)
 results = simulation.run()
 solution = results.get_solution()
 
-# W ogóle w zmiennej results są rzeczy
-# które można wyprintować za pomocą matplotliba (np historia funkcji fitness kazdej iteracji)
-# Przykład użycia jest w moim projekcie na MMWD (app.py czy jakoś tak, klasa cośtam z Results w nazwie)
-
 print('Best iteration:', results.best_iteration())
-print(solution)
+print('Solution:', solution)
 print("Algorithm total time: {:.2f}s".format(results.totalTime))
 
+
+def plot_fitness_history(config: Config, result: AlgorithmResult):
+    MAX_RUNS = config['max_runs']
+    best_index = result.best_iteration()
+    plt.figure()
+    plt.title("Generation fitness value")
+    plt.plot(range(1,MAX_RUNS+1), result.max_values, marker='.', label=r"Best")
+    plt.plot(range(1,MAX_RUNS+1), result.avg_values, marker='.', label=r"Average")
+    plt.plot([best_index+1], result.max_values[best_index], marker='o', color='r')
+    plt.legend(loc='lower right')
+    plt.grid()
+    plt.xlabel("Generation")
+    plt.ylabel("Fitness")
+
+
+def plot_portfolio_history(config: Config, result: AlgorithmResult):
+    MAX_RUNS = config['max_runs']
+    plt.figure()
+    plt.title("Generation best portfolio distribution")
+
+    for i, name in enumerate(result.gene_names):
+        plt.plot(range(1, MAX_RUNS + 1), [x.portfolio[i] for x in result.champion_chromosomes], marker='.', label=name)
+        pass
+
+    plt.legend(loc='center right')
+    plt.grid()
+    plt.xlabel("Generation")
+    plt.ylabel("Portfolio distribution")
+
+
+def plot_solution(result: AlgorithmResult):
+    solution = result.get_solution()
+    labels = solution.keys()
+    sizes = solution.values()
+
+    plt.figure()
+    plt.title("Final portfolio distribution")
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+            startangle=90)
+    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+
+plot_fitness_history(cfg, results)
+plot_portfolio_history(cfg, results)
+plot_solution(results)
+plt.show()
