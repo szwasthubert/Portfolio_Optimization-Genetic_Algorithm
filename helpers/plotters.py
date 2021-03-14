54516d18320b82f177fc7d223649d1cf140f4e6d
Filip Frey <freyfilip3@gmail.com>
2021-03-14 19:17:13 +0100

    plotters arranging

---
+++ helpers/plotters.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as spo
+from typing import List, Dict, Tuple
+from helpers import helper as help
+from genetic import algorithm as algo
+
+
+def get_plot_color(index: int, cmap:str) -> str:
+    return plt.cm.get_cmap(cmap)(index)
+
+
+def efficient_frontier_plotter(risk_return_dicts_list: List[Dict[str, List]]):
+
+    for index, risk_return_dict in enumerate(risk_return_dicts_list):
+        stds, returns = zip(*risk_return_dict.values())
+        stds = list(map(list, stds))[0]
+        returns = list(map(list,returns))[0]
+        plt.scatter(stds, returns, marker="X", label=list(risk_return_dict.keys())[0], color=get_plot_color(1+index*2, 'Paired'))
+        popt, pcov = spo.curve_fit(help.fitfun, stds, returns)
+        t = np.arange(min(stds), max(stds), 0.1)
+        plt.plot(t, help.fitfun(np.array(t), *popt), '-', label='_nolegend_', color=get_plot_color(index*2, 'Paired'))
+        plt.legend()
+        plt.grid(True)
+        plt.xlabel("Portfolio risk")
+        plt.ylabel("Expected return")
+        plt.title("Markowitz Model Efficient Frontiers")
+        plt.savefig('./plots/frontier/'+str(help.get_num_files_in_dir('./plots/frontier/'))+'.png')
+
+    return plt.show()
+
+
+def plot_fitness_history(config: algo.Config, result: algo.AlgorithmResult):
+    MAX_RUNS = config['max_runs']
+    best_index = result.best_iteration()
+    plt.figure()
+    plt.title("Generation fitness value")
+    plt.plot(range(1,MAX_RUNS+1), result.max_values, marker='.', label=r"Best")
+    plt.plot(range(1,MAX_RUNS+1), result.avg_values, marker='.', label=r"Average")
+    plt.plot([best_index+1], result.max_values[best_index], marker='o', color='r')
+    plt.legend(loc='lower right')
+    plt.grid()
+    plt.xlabel("Generation")
+    plt.ylabel("Fitness")
+    plt.savefig('./plots/fitnesshist/' + str(help.get_num_files_in_dir('./plots/fitnesshist/')) + '.png')
+
+
+def plot_portfolio_history(config: algo.Config, result: algo.AlgorithmResult):
+    MAX_RUNS = config['max_runs']
+    plt.figure()
+    plt.title("Generation best portfolio distribution")
+
+    for i, name in enumerate(result.gene_names):
+        plt.plot(range(1, MAX_RUNS + 1), [x.portfolio[i] for x in result.champion_chromosomes], marker='.', label=name)
+        pass
+
+    plt.legend(loc='center right')
+    plt.grid()
+    plt.xlabel("Generation")
+    plt.ylabel("Portfolio distribution")
+    plt.savefig('./plots/distrib/' + str(help.get_num_files_in_dir('./plots/distrib/')) + '.png')
+
+
+def plot_solution(result: algo.AlgorithmResult):
+    solution = result.get_solution()
+    labels = solution.keys()
+    sizes = solution.values()
+
+    plt.figure()
+    plt.title("Final portfolio distribution")
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+            startangle=90)
+    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.savefig('./plots/solution/' + str(help.get_num_files_in_dir('./plots/solution/')) + '.png')
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import numpy as np
 from config import config as cfg, Config
 from genetic.operators import OperatorsImpl
 from genetic.algorithm import GeneticAlgorithm, AlgorithmParams, AlgorithmResult
-
+from helpers import plotters as plot
 import matplotlib.pyplot as plt
 
 # przykladowe liczby z dupy wziete
-params_crypto = AlgorithmParams(lam=0.5,
+params_crypto = AlgorithmParams(lam=0.67,
                                 R=np.array([1,2,3,4]),
                                 V=np.ones(shape=(4,4)),
                                 chromosome_size=4,
@@ -22,48 +22,7 @@ print('Solution:', solution)
 print("Algorithm total time: {:.2f}s".format(results.totalTime))
 
 
-def plot_fitness_history(config: Config, result: AlgorithmResult):
-    MAX_RUNS = config['max_runs']
-    best_index = result.best_iteration()
-    plt.figure()
-    plt.title("Generation fitness value")
-    plt.plot(range(1,MAX_RUNS+1), result.max_values, marker='.', label=r"Best")
-    plt.plot(range(1,MAX_RUNS+1), result.avg_values, marker='.', label=r"Average")
-    plt.plot([best_index+1], result.max_values[best_index], marker='o', color='r')
-    plt.legend(loc='lower right')
-    plt.grid()
-    plt.xlabel("Generation")
-    plt.ylabel("Fitness")
-
-
-def plot_portfolio_history(config: Config, result: AlgorithmResult):
-    MAX_RUNS = config['max_runs']
-    plt.figure()
-    plt.title("Generation best portfolio distribution")
-
-    for i, name in enumerate(result.gene_names):
-        plt.plot(range(1, MAX_RUNS + 1), [x.portfolio[i] for x in result.champion_chromosomes], marker='.', label=name)
-        pass
-
-    plt.legend(loc='center right')
-    plt.grid()
-    plt.xlabel("Generation")
-    plt.ylabel("Portfolio distribution")
-
-
-def plot_solution(result: AlgorithmResult):
-    solution = result.get_solution()
-    labels = solution.keys()
-    sizes = solution.values()
-
-    plt.figure()
-    plt.title("Final portfolio distribution")
-    plt.pie(sizes, labels=labels, autopct='%1.1f%%',
-            startangle=90)
-    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-
-plot_fitness_history(cfg, results)
-plot_portfolio_history(cfg, results)
-plot_solution(results)
+plot.plot_fitness_history(cfg, results)
+plot.plot_portfolio_history(cfg, results)
+plot.plot_solution(results)
 plt.show()
