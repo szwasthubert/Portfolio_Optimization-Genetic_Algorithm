70e84bd6d5541e0e4a9c2d526794d0801199470d
Filip Frey <freyfilip3@gmail.com>
2021-03-24 18:31:02 +0100

    plotter fix

--- helpers/helper.py
+++ helpers/helper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import math
+import numpy as np
 import pandas as pd
 import os
 from typing import Dict, List, Tuple
@@ -11,19 +11,20 @@ def calculate_diff(dataframe: pd.DataFrame) -> pd.DataFrame:
 
 
 def calculate_mean(dataframe: pd.DataFrame) -> pd.DataFrame:
-    return dataframe.mean(axis=0, skipna=True)
+    return dataframe.mean(skipna=True)
 
 
 def calculate_std(dataframe: pd.DataFrame) -> pd.DataFrame:
-    return dataframe.std(axis=0, skipna=True)
+    return dataframe.std(skipna=True)
 
 
-def calculate_covariance_matrix(dataframe: pd.DataFrame) -> pd.DataFrame:
-    return dataframe.cov()
+def calculate_normalized_covariance_matrix(dataframe: pd.DataFrame) -> pd.DataFrame:
+    return dataframe.corr()
 
 
 def calculate_RV(dataframe: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    return calculate_mean(calculate_diff(dataframe)), calculate_covariance_matrix(dataframe)
+    dataframe = calculate_diff(dataframe)
+    return calculate_mean(dataframe), calculate_normalized_covariance_matrix(dataframe)
 
 
 def expected_portfolio_return(portfolioWeightsDict: Dict, meanReturnsDataset: pd.DataFrame) -> float:
@@ -38,14 +39,14 @@ def portfolio_std(portfolioWeightsDict: Dict, covarianceDataset: pd.DataFrame) -
     portfolioKeys = list(portfolioWeightsDict.keys())
     variance = 0
     for key, value in portfolioWeightsDict.items():
-        variance += covarianceDataset.loc[key][key] * value ** 2
+        variance += covarianceDataset.loc[key][key] * (value ** 2)
 
-    for i in range(n):
-        for j in range(i + 1, n + 1):
+    for i in range(n-1):
+        for j in range(i+1, n):
             variance += 2 * portfolioWeightsDict[portfolioKeys[i]] * portfolioWeightsDict[portfolioKeys[j]] * \
-                        covarianceDataset.iloc[portfolioKeys[i]][portfolioKeys[j]]
+                        covarianceDataset.loc[portfolioKeys[i]][portfolioKeys[j]]
 
-    return math.sqrt(variance)
+    return variance
 
 
 def risk_return_dict_generator(instrumentSet: str, portfolioWeightsDicts: List[Dict], meanReturnsDataset: pd.DataFrame,
@@ -56,10 +57,7 @@ def risk_return_dict_generator(instrumentSet: str, portfolioWeightsDicts: List[D
 
 
 def fitfun(x, a, b, c, d):
-    """
-    Hyperbolic curve equation
-    """
-    return d + a / (b * x + c)
+    return d + a/(b*x+c)
 
 
 def get_num_files_in_dir(path: str) -> int:
