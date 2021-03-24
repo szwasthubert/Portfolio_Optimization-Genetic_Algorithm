e2578baa5ede9b542096711d128ed3da01b343f4
Filip Frey <freyfilip3@gmail.com>
2021-03-24 18:31:37 +0100

    plotters fix

--- helpers/plotters.py
+++ helpers/plotters.py
@@ -15,13 +15,18 @@ def get_plot_color(index: int, cmap:str) -> str:
 def efficient_frontier_plotter(risk_return_dicts_list: List[Dict[str, List]]):
 
     for index, risk_return_dict in enumerate(risk_return_dicts_list):
-        stds, returns = zip(*risk_return_dict.values())
-        stds = list(map(list, stds))[0]
-        returns = list(map(list,returns))[0]
+        plt.figure(2*index)
+        stds = []
+        returns = []
+        for tup in list(risk_return_dict.values())[0]:
+            stds.append(tup[0])
+            returns.append(tup[1])
         plt.scatter(stds, returns, marker="X", label=list(risk_return_dict.keys())[0], color=get_plot_color(1+index*2, 'Paired'))
-        popt, pcov = spo.curve_fit(help.fitfun, stds, returns)
-        t = np.arange(min(stds), max(stds), 0.1)
-        plt.plot(t, help.fitfun(np.array(t), *popt), '-', label='_nolegend_', color=get_plot_color(index*2, 'Paired'))
+        #popt, pcov = spo.curve_fit(help.fitfun, stds, returns)
+
+
+        t = np.linspace(min(stds), max(stds), 5000)
+        #plt.plot(t, help.fitfun(np.array(t), *popt), '-', label='_nolegend_', color=get_plot_color(index*2, 'Paired'))
         plt.legend()
         plt.grid(True)
         plt.xlabel("Portfolio risk")
